f0e0ee974574a7803a7c7ddeae3361298e242576
mohammad molavi <mohamadmolavi98@gmail.com>
2024-04-06 13:59:40 +0430

    develop tablecoin component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
 
-function TableCoin({coins}) {
-    console.log(coins)
+function TableCoin({ coins }) {
   return (
     <div>
-      Table Coin
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <TableRow coin={coin} key={coin.id} />
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default TableCoin
+export default TableCoin;
+
+const TableRow = ({ coin: { name, image, symbol, total_volume, current_price, price_change_percentage_24h: price_change } }) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
